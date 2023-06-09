c229e9d0f4613842aa885cb71eadde4d5596f93d
AranBeitia <arantza.beitia@gmail.com>
2023-06-09 20:21:05 +0200

    create boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import ListItems from './components/ListItems/ListItems'
-// import WelcomeC from './components/welcomeC/welcomeC'
-// import WelcomeF from './components/welcomeF/welcomeF'
-
-// const Welcome = () => {
-// 	return <h3>Hello The Bridge</h3>
-// }
-
-const name = 'Pepe'
-
-const users = [
-	{ name: 'Carlos', email: 'carlos@mail.com' },
-	{ name: 'Pepin', email: 'pepin@mail.com' },
-	{ name: 'Ane', email: 'ane@mail.com' },
-	{ name: 'June', email: 'june@mail.com' },
-]
 
 function App() {
-	return (
-		<div>
-			<h1 className="text-color">hola</h1>
-			<h2>name: {name}</h2>
-			<ListItems items={users} />
-			{/* <Welcome />
-			<WelcomeC name="Carlos" />
-			<WelcomeC name="Maria" />
-			<WelcomeC name="Ane" />
-			<WelcomeC name="Pepelu" />
-			<WelcomeC name="June" />
-			<WelcomeF firstName="Sandro" />
-			<WelcomeF firstName="Sandra" />
-			<WelcomeF firstName="Sandrine" /> */}
-		</div>
-	)
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
