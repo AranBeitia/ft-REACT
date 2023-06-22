69e5fd1524839f65435ce3e418ca278366741e29
AranBeitia <arantza.beitia@gmail.com>
2023-06-22 18:13:50 +0200

    Feat: authentication

---
+++ src/components/TheHeader/TheHeader.jsx
@@ -0,0 +1,45 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { UserContext } from '../../context/UserContext/UserState'
+
+import { Menu } from 'antd'
+import { LogoutOutlined, LoginOutlined, UserOutlined } from '@ant-design/icons'
+
+const TheHeader = () => {
+	const { token, logout } = useContext(UserContext)
+	const navigate = useNavigate()
+
+	const logoutUser = () => {
+		logout()
+		setTimeout(() => {
+			navigate('/')
+		}, 2000)
+	}
+
+	return (
+		<nav className="header">
+			<Menu mode="horizontal" defaultSelectedKeys={['profile']}>
+				{token ? (
+					<>
+						<Menu.Item
+							key="logout"
+							icon={<LogoutOutlined />}
+							onClick={logoutUser}
+						>
+							<Link to="/">Logout</Link>
+						</Menu.Item>
+						<Menu.Item key="profile" icon={<UserOutlined />}>
+							<Link to="/profile">Profile</Link>
+						</Menu.Item>
+					</>
+				) : (
+					<Menu.Item key="login" icon={<LoginOutlined />}>
+						<Link to="/">Login</Link>
+					</Menu.Item>
+				)}
+			</Menu>
+		</nav>
+	)
+}
+
+export default TheHeader
